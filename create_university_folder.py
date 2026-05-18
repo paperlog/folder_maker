aa7261f7b010cbd39655add22d7bc9dfd80579ad
paperlog <yuu625581@gmail.com>
2026-05-18 15:40:11 +0900

    deadlineの削除、「授業」「未提出」フォルダ内に16個(１セメスター分）のフォルダ作成を行うようにした。

--- create_university_folder.py
+++ create_university_folder.py
@@ -4,25 +4,25 @@ import shutil
 def create_folder(folder_name, folder_type="sub"):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-        print(f"{folder_name} を作成しました")
-    else:
-        print(f"{folder_name} はすでに存在します")
     
     if folder_type == "sub":
-        subfolders = ["授業", "提出済み", "未提出"]
+        subfolders = ["授業", "未提出", "提出済み"]
         for subfolder in subfolders:
             create_folder(os.path.join(folder_name, subfolder), folder_type="none")
+            if subfolder == "授業":
+                create_folder_numbers("class", os.path.join(folder_name, subfolder), 16)
+            elif subfolder == "未提出":
+                create_folder_numbers("file", os.path.join(folder_name, subfolder), 16)
+def create_folder_numbers(name,folder_name,numbers):
+    for i in range(1, numbers+1):
+        n=name+str(i)
+        create_folder(os.path.join(folder_name, n), folder_type="none")
 
 if __name__ == "__main__":
     folder_list = []
     top_folder = "example"
     create_folder(top_folder, folder_type="top")
     files = os.listdir('.')
-    try:
-        shutil.copy("deadline.py", top_folder)
-        print("deadline.pyをexample内に作成しました")
-    except FileNotFoundError:
-        print("deadline.pyが見つかりませんでした。deadline.pyの複製は行われません")
     while True:
         print(f"フォルダーリスト:{folder_list}")
         class_name = input("授業名を入力してください。入力を終了する際には[end]と入力してください: ")
